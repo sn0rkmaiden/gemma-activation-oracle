e854f3e9265445c27495ae193e4d628872e829c1
sn0rkmaiden <alisapetrova275@gmail.com>
2026-02-07 20:37:12 +0300

    fixed not finding modules when running script

--- ao/train_phase_a.py
+++ ao/train_phase_a.py
@@ -12,8 +12,8 @@ from torch.optim import AdamW
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
 
-from .data_wiki import stream_wikipedia, PackedWiki, collate_phase_a
-from .runtime import ActivationOracleRuntime
+from ao.data_wiki import stream_wikipedia, PackedWiki, collate_phase_a
+from ao.runtime import ActivationOracleRuntime
 
 
 def ensure_single_token_placeholder(tok, candidate: str = "?"):
